32a5c323c418ea97f73508d541d9065af9ee33e6
dec1218 <decflorhenuy.202301455@gmail.com>
2024-10-21 07:18:14 +0000

    updatesss

---
+++ script.js
@@ -0,0 +1,12 @@
+const products = [
+    {"id": 1, "name": "Laptop", "price": 1500, "stock": 5},
+    {"id": 2, "name": "Headphones", "price": 100, "stock": 10},
+    {"id": 3, "name": "Keyboard", "price": 50, "stock": 20}
+  ];
+
+  function alertProductDetails(productId) {
+    const product = products.find(p => p.id === productId);
+    if (product) {
+      alert(Product: ${product.name}\nPrice: $${product.price}\nIn Stock: ${product.stock});
+    }
+  }
